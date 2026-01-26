80b08f89270b3af7ab26498a7db9316edb5b0557
sukiSophie <s76211152@gmail.com>
2026-01-26 21:23:23 +0800

    修改配置

--- src/.config/user.ts
+++ src/.config/user.ts
@@ -1,7 +1,38 @@
 import type { UserConfig } from '~/types'
 
 export const userConfig: Partial<UserConfig> = {
-  // Override the default config here
-  // site: { title: "講評世界" },
-  // seo: { twitter: "@moeyua13" },
+  site: {
+    title: 'sukiSophie的小站',
+    subtitle: 'secret-base',
+    author: 'sukiSophie',
+    description: 'sharing feelings and thoughts',
+    website: 'https://suki37.dpdns.org',
+    socialLinks: [
+      { name: 'github', href: 'https://github.com/sukiSophie' },
+      { name: 'rss', href: '/atom.xml' },
+    ],
+    navLinks: [
+      { name: 'Posts', href: '/' },
+      { name: 'Archive', href: '/archive' },
+      { name: 'Categories', href: '/categories' },
+      { name: 'About', href: '/about' },
+    ],
+    categoryMap: [
+      { name: '项目', path: 'proj' },
+      { name: '技术', path: 'tech' },
+      { name: '观点', path: 'view' },
+      { name: '学习', path: 'learn' },
+      { name: '生活', path: 'life' },
+    ],
+  },
+  appearance: {
+    theme: 'system',
+    locale: 'zh-cn',
+  },
+  latex: {
+    katex: true,
+  },
+  rss: {
+    fullText: true,
+  },
 }
